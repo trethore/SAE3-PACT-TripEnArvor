46b14ce84b3b64280227b31bf9d3ce424605a697
Pikoow <pikdev.gopi@gmail.com>
2024-10-22 15:12:14 +0200

    test de select avec le serveur

--- html/se-connecter-pro/index.php
+++ html/se-connecter-pro/index.php
@@ -1,3 +1,22 @@
+<?php
+include('php/connect_params.php');
+try {
+    $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $stmt = $dbh->prepare('SELECT email from _compte');
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    echo "<pre>";
+    print_r($result);
+    echo "</pre>";
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
