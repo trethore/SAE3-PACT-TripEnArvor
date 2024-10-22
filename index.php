614159d70f28b658e65d70b0d19eccc67c19a7fa
MatheoPf <matheo.pfranger@gmail.com>
2024-10-22 14:57:12 +0200

    ajout connection params

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
 
-// Connexion à la BDD
-$conn = new mysqli('localhost', 'username', 'password', 'database_name');
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+include('connect_params_perso.php');
+try {
+    $dbh = new PDO("$driver:host=$server;dbname=$dbname", 
+            $user, $pass);
+    foreach($dbh->query('SELECT * from forum1._user', 
+                        PDO::FETCH_ASSOC) 
+                as $row) {
+        echo "<pre>";
+        print_r($row);
+        echo "</pre>";
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
 }
 
 $sql = "SELECT ville, titre, typeOffre, nombreAvis, prix FROM Offre";
