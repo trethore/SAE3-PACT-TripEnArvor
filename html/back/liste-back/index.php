530f3628b89876688df291264b040a6919453d55
MatheoPf <matheo.pfranger@gmail.com>
2024-10-23 21:51:17 +0200

    ajout header footer

--- html/back/liste-back/index.php
+++ html/back/liste-back/index.php
@@ -84,9 +84,21 @@ $result = $conn->query($reqOffre);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/style/style_backListe.css">
+    <link rel="stylesheet" href="/style/styles.css">
+    <link rel="stylesheet" href="/style/style_HFB.css">
     <title>Liste de vos offres</title>
 </head>
 <body>
+    <header>
+        <img class="logo" src="/images/universel/logo/Logo_blanc.png" />
+        <div class="text-wrapper-17">PACT Pro</div>
+        <div class="search-box">
+        <button class="btn-search"><img class="cherchero" src="/images/universel/icones/chercher.png" /></button>
+        <input type="text" class="input-search" placeholder="Taper votre recherche...">
+        </div>
+        <a href="index.html"><img class="ICON-accueil" src="/images/universel/icones/icon_accueil.png" /></a>
+        <a href="index.html"><img class="ICON-utilisateur" src="/images/universel/icones/icon_utilisateur.png" /></a>
+  </header>
     <main>
         <h1>Liste de vos offre</h1>
         <!--------------- 
@@ -304,5 +316,40 @@ $result = $conn->query($reqOffre);
         </div>
         </section>
     </main>
+    <footer>
+        <div class="footer-top">
+        <div class="footer-top-left">
+            <span class="footer-subtitle">P.A.C.T</span>
+            <span class="footer-title">TripEnArmor</span>
+        </div>
+        <div class="footer-top-right">
+            <span class="footer-connect">Restons connectés !</span>
+            <div class="social-icons">
+            <a href="https://x.com/?locale=fr">
+                <div class="social-icon" style="background-image: url('/images/universel/icones/x.png');"></div>
+            </a>
+            <a href="https://www.facebook.com/?locale=fr_FR">
+                <div class="social-icon" style="background-image: url('/images/universel/icones/facebook.png');"></div>
+            </a>
+            <a href="https://www.youtube.com/">
+                <div class="social-icon" style="background-image: url('/images/universel/icones/youtube.png');"></div>
+            </a>
+            <a href="https://www.instagram.com/">
+                <div class="social-icon" style="background-image: url('/images/universel/icones/instagram.png');"></div>
+            </a>
+            </div>
+        </div>
+
+
+        <!-- Barre en bas du footer incluse ici -->
+
+        </div>
+        <div class="footer-bottom">
+        Politique de confidentialité - Politique RGPD - <a href="mention_legal.html">Mentions légales</a> - Plan du site -
+        Conditions générales - ©
+        Redden's, Inc.
+        </div>
+    </footer>
+    <?php session_destroy();?>
 </body>
 </html>
