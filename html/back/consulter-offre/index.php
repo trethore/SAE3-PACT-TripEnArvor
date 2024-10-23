1806b15847abdec2390ef1737240a90eb758ab97
StanislasROLLAND <stanislasrolland05@gmail.com>
2024-10-23 17:24:27 +0200

    petit commit avant de faire nimp

--- html/back/consulter-offre/index.php
+++ html/back/consulter-offre/index.php
@@ -1,3 +1,45 @@
+<?php
+include('/connect_params.php');
+try {
+    $dbh = new PDO("$driver:host=$server;dbname=$dbname", 
+            $user, $pass);
+    foreach($dbh->query('SELECT * from forum1._user', 
+                        PDO::FETCH_ASSOC) 
+                as $row) {
+        echo "<pre>";
+        print_r($row);
+        echo "</pre>";
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+/*******************
+Requete SQL préfaite
+********************/
+$reqOffre = "SELECT * FROM _offre";
+$reqIMG = "SELECT img.lien_fichier 
+            FROM _image img
+            JOIN _offre_contient_image oci 
+            ON img.lien_fichier = oci.id_image
+            WHERE oci.id_offre = $id_offre_cible
+            LIMIT 1;";
+$reqTypeOffre = $sql = "SELECT 
+                        CASE
+                            WHEN EXISTS (SELECT 1 FROM _offre_restauration r WHERE r.id_offre = o.id_offre) THEN 'Restauration'
+                            WHEN EXISTS (SELECT 1 FROM _offre_parc_attraction p WHERE p.id_offre = o.id_offre) THEN 'Parc d\'attraction'
+                            WHEN EXISTS (SELECT 1 FROM _offre_spectacle s WHERE s.id_offre = o.id_offre) THEN 'Spectacle'
+                            WHEN EXISTS (SELECT 1 FROM _offre_visite v WHERE v.id_offre = o.id_offre) THEN 'Visite'
+                            WHEN EXISTS (SELECT 1 FROM _offre_activite a WHERE a.id_offre = o.id_offre) THEN 'Activité'
+                            ELSE 'Inconnu'
+                        END AS offreSpe
+                        FROM _offre o
+                        WHERE o.id_offre = ?";
+$result = $conn->query($reqOffre); 
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -35,10 +77,14 @@
             <div id="confirm">
                 <p>Voulez-vous mettre votre offre hors ligne ?</p>
                 <div class="close">
-                    <button onclick="closeConfirmConfirmer()">Mettre hors ligne</button>
-                    <button onclick="closeConfirmAnnuler()">Annuler</button>
+                    <button onclick="showFinal()">Mettre hors ligne</button>
+                    <button onclick="btnAnnuler()">Annuler</button>
                 </div>
             </div>
+            <div id="final">
+                <p>Offre hors ligne !<br>Cette offre n'apparait plus</p>
+                <button onclick="btnAnnuler()">Fermer</button>
+            </div> 
             <button id="bouton1" onclick="showConfirm()">Mettre hors ligne</button>
             <button id="bouton2">Modifier l'offre</button>
         </div>
@@ -47,7 +93,7 @@
     <main id="body">
         <section class="fond-blocs"><!---->
 
-            <h1>Côté Plage</h1>
+            <h1><?php echo $offre["titre"] ?></h1>
             <div class="galerie-images-presentation"> 
                 <img src="/images/universel/photos/hotel_2.png" alt="Image 1">
                 <img src="/images/universel/photos/hotel_2_2.png" alt="Image 2">
@@ -57,8 +103,8 @@
             </div>
 
             <div class="display-ligne-espace">
-                <p><em>Restaurant, Ouvert</em></p>
-                <p>1 Rue Koz Ker, 56370 SARZEAU</p>
+                <p><em><?php echo $offre["categorie"] . ", " . $offre["ouvert"] ?></em></p>
+                <p><?php echo $offre["adresse"] . ", " . $offre["ville"] ?></p>
             </div>
                 
             <div class="display-ligne">
@@ -67,12 +113,12 @@
                 <img src="/images/universel/icones/etoile-jaune.png" class="etoile">
                 <img src="/images/universel/icones/etoile-jaune.png" class="etoile">
                 <img src="/images/universel/icones/etoile-jaune.png" class="etoile">
-                <p>(49)</p>
+                <p>(<?php echo $offre["nombre_avis"] ?>)</p>
                 <a href="#avis">Voir les avis</a>
             </div>
 
             <div class="display-ligne-espace">
-                <p>Proposée par : David Guillet</p>
+                <p>Proposée par : <?php echo $offre["nom_pro"] ?></p>
                 <button>Voir les tarifs</button>
             </div>
 
@@ -80,7 +126,7 @@
 
         <section class="double-blocs"><!---->
 
-            <div class="fond-blocs bloc-caracteristique">
+            <div id="caracteristiques" class="fond-blocs bloc-caracteristique">
                 <ul class="liste-caracteristique">
                     <li><img src="/images/universel/icones/cuisine.png"><h2>Cuisine traditionnelle</h2></li>
                     <li><img src="/images/universel/icones/mer.png"><h2>Vue sur mer</h2></li>
@@ -89,10 +135,10 @@
             </div> 
 
             <div class="fond-blocs bloc-a-propos">
-                <h2>À propos de Côté Plage :</h2>
-                <p>Repris en 2010, le restaurant propose une cuisine traditionnelle axée sur les poissons et fruits de mer, mettant en avant des produits locaux et de saison. Depuis sa reprise, David Guillet a renouvelé l'équipe et introduit de nouvelles initiatives, telles qu'une terrasse lounge pour les apéritifs, dans une ambiance conviviale et les pieds dans l’eau​</p>
-                <a href="https://les-embruns-du-phare.fr"><img src="/images/universel/icones/lien.png" alt="epingle" class="epingle">restaurant-cote-plage.fr</a>
-                <p>Numéro : 04 94 66 97 64</p>
+                <h2>À propos de : <?php echo $offre["titre"] ?></h2>
+                <p><?php echo $offre["a_propos"] ?>​</p>
+                <a href="<?php echo $offre["site"] ?>"><img src="/images/universel/icones/lien.png" alt="epingle" class="epingle"><?php echo $offre["site"] ?></a>
+                <p>Numéro : <?php echo $offre["tel"] ?></p>
             </div>
     
         </section>
@@ -100,9 +146,8 @@
         <section class="fond-blocs"><!---->
 
             <h2>Description détaillée de l'offre :</h2>
-            <p>Les pieds dans l’eau sur la plage du Roaliguen. Nous proposons une cuisine de caractère, inventive à base de produits locaux et de saison. Tout cela dans une ambiance conviviale.</p>
-            <p>Nouveau : Terrasse Lounge pour vos moments apéritif.</p>
-            <p>Ouvert d'avril à novembre.<br>Fermé le mercredi en juillet-août.<br>Fermé le mercredi et le jeudi hors saison.</p>
+            <p><?php echo $offre["desc"] ?></p>
+            <p><?php echo $offre["desc2"] ?></p>
 
         </section>
 
@@ -112,14 +157,21 @@
                 <div>
                     <h2>Tarifs :</h2>
                     <table>
-                        <tr>
-                            <td>Dunburi de Poulet 22 €</td>
-                            <td>Burger de Boeuf 24,50 €</td>
-                        </tr>
-                        <tr>
-                            <td>Menu Petit Plagiste 11,80 €</td>
-                            <td>Veggie Bowl 18 €</td>
-                        </tr>
+                        <?php
+                        $counter = 0;
+                        echo "<tr>";
+                        foreach ($offre["tarifs"] as $price => $description) {
+                            echo "<td>$price $description</td>";
+                            $counter++;
+
+                            if ($counter % 2 == 0) {
+                                echo "</tr><tr>";
+                            }
+                        }
+                        if ($counter % 2 != 0) {
+                            echo "</tr>";
+                        }
+                        ?>
                     </table>
                 </div>
 
@@ -129,26 +181,26 @@
             <div class="fond-blocs bloc-ouverture">
                 <h2>Ouverture :</h2>
                 <ul>
-                    <li><em>Lundi : 09h - 23h</em></li>
-                    <li><em>Mardi : 09h - 23h</em></li>
-                    <li><em>Mercredi : 09h - 23h</em></li>
-                    <li><em>Jeudi : 09h - 23h</em></li>
-                    <li><em>Vendredi : 09h - 23h</em></li>
-                    <li><em>Samedi : 09h - 23h</em></li>
-                    <li><em>Dimanche : 09h - 20h</em></li>
+                    <li><em>Lundi : <?php echo $offre["horaires"]["Lundi"] ?></em></li>
+                    <li><em>Mardi : <?php echo $offre["horaires"]["Mardi"] ?></em></li>
+                    <li><em>Mercredi : <?php echo $offre["horaires"]["Mercredi"] ?></em></li>
+                    <li><em>Jeudi : <?php echo $offre["horaires"]["Jeudi"] ?></em></li>
+                    <li><em>Vendredi : <?php echo $offre["horaires"]["Vendredi"] ?></em></li>
+                    <li><em>Samedi : <?php echo $offre["horaires"]["Samedi"] ?></em></li>
+                    <li><em>Dimanche : <?php echo $offre["horaires"]["Dimanche"] ?></em></li>
                 </ul>
             </div> 
     
         </section>
 
-        <section class="fond-blocs">
+        <section id="carte" class="fond-blocs">
 
             <h1>Localisation</h1>
             <div id="map" class="carte"></div>
 
         </section>
 
-        <section class="fond-blocs avis">
+        <section id="avis" class="fond-blocs avis">
 
             <div class="display-ligne">
                 <h2>Note moyenne :</h2>
@@ -253,6 +305,7 @@
     <script>
 
         let confirmDiv = document.getElementById("confirm");
+        let finalDiv = document.getElementById("final");
 
         function showConfirm() {
             confirmDiv.style.display = "block";
@@ -271,11 +324,14 @@
         }
 
         function showFinal() {
-            confirmDiv.style.display = "block";
+            finalDiv.style.display = "block";
+            confirmDiv.style.display = "none";
+            popup.style.filter = "none";
         }
 
-        function closeConfirmAnnuler() {
+        function btnAnnuler() {
             confirmDiv.style.display = "none";
+            finalDiv.style.display = "none";
             let header = document.getElementById('header');
             header.style.filter = "blur(0px)";
             let body = document.getElementById('body');
@@ -287,23 +343,6 @@
             let bouton2 = document.getElementById('bouton2');
             bouton2.style.filter = "blur(0px)";
         }
-
-        function closeConfirmConfirmer() {
-            confirmDiv.style.display = "none";
-            let header = document.getElementById('header');
-            header.style.filter = "blur(0px)";
-            let body = document.getElementById('body');
-            body.style.filter = "blur(0px)";
-            let footer = document.getElementById('footer');
-            footer.style.filter = "blur(0px)";
-            let bouton1 = document.getElementById('bouton1');
-            bouton1.style.filter = "blur(0px)";
-            let bouton2 = document.getElementById('bouton2');
-            bouton2.style.filter = "blur(0px)";
-            alert("Offre hors ligne");
-        }
-
-        function 
 
     </script>
 
