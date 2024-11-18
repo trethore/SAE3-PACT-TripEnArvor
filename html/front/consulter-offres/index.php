c7a5d83be37fb846b61e999016678887f0dd4b03
Pikoow <pikdev.gopi@gmail.com>
2024-11-18 09:37:17 +0100

    changement lien css

--- html/front/consulter-offres/index.php
+++ html/front/consulter-offres/index.php
@@ -217,7 +217,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consulter vos offres</title>
-    <link rel="stylesheet" href="../../style.css">
+    <link rel="stylesheet" href="/html/style/style-consulter-offres-front.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 </head>
