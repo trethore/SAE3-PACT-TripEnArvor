ece408eede329b22b359c8364da3ac0f10dd7cc6
trethore <titou.rethore@gmail.com>
2024-11-20 15:46:25 +0100

    changes to se connecter no more infinite load

--- html/back/se-connecter/index.php
+++ html/back/se-connecter/index.php
@@ -54,32 +54,10 @@ try {
 
             if ($trouve) {
                 if (isIdMember($id)) {
-                ?>
-                    <script>
-                        window.onload = function() {
-                            window.location.href = 'https://redden.ventsdouest.dev/front/consulter-offres/';
-                        };
-                    </script>
-                <?php
+                    redirectTo('https://redden.ventsdouest.dev/front/consulter-offres/');
                 } else if (isIdProPrivee($id) || isIdProPublique($id)) {
-                ?>
-                    <script>
-                        window.onload = function() {
-                            window.location.href = 'https://redden.ventsdouest.dev/back/liste-back/';
-                        };
-                    </script>
-                <?php
-                } else {
-                    ?>
-                    <script>
-                        setTimeout(() => {
-                            window.location.reload();
-                        }, 100);
-                    </script>
-                <?php
+                    redirectTo('https://redden.ventsdouest.dev/back/liste-back/');
                 }
-                ?>
-                <?php
             } else {
                 ?>
                     <script>
