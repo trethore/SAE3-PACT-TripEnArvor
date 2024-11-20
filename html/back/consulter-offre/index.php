24ee02007a7b266dae9d0f642be16b43a795d893
StanislasROLLAND <stanislasrolland05@gmail.com>
2024-11-20 16:30:32 +0100

    C'est nul

--- html/back/consulter-offre/index.php
+++ html/back/consulter-offre/index.php
@@ -100,8 +100,6 @@ try {
             <div class="carousel">
                 <div class="carousel-images">
                     <img src="/images/hotel_2.png" alt="Image 1">
-                    <img src="/images/hotel-2-1.png" alt="Image 2">
-                    <img src="/images/hotel_2-3.png" alt="Image 3">
                 </div>
                 <button class="prev"><img src="/images/fleche-navigation-6-1.png" alt="Flèche navigation"></button>
                 <button class="next"><img src="/images/fleche-navigation-5-1.png" alt="Flèche navigation"></button>
