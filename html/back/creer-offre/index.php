b2223b6463e10028d011e2239d279ad2c4555fac
camille <camille.poupon@etudiant.univ-rennes.fr>
2024-11-25 18:17:35 +0000

    enlevage du js sur les rb (c'etaitde la merde)

--- html/back/creer-offre/index.php
+++ html/back/creer-offre/index.php
@@ -585,15 +585,6 @@ if (isset($_POST['titre'])) {
         // Sélectionner tous les boutons radio
         let radioButtons = document.querySelectorAll('input[type="radio"][name="option"]');
 
-        // Ajouter un événement sur chaque bouton radio
-        radioButtons.forEach((radio) => {
-            radio.addEventListener('click', function() {
-                // Si le bouton est déjà coché, désélectionnez-le
-                if (this.checked) {
-                    this.checked = false;
-                }
-            });
-        });
 
         let typecategorie = document.getElementById('categorie');
         let typerestaurant = ["carte", "labelcarte","labelgammedeprix", "gammedeprix"];
