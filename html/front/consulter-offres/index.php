edd091ba449810ed03229fdabbbec8a61ee2baeb
Pikoow <pikdev.gopi@gmail.com>
2024-11-25 13:49:47 +0100

    test

--- html/front/consulter-offres/index.php
+++ html/front/consulter-offres/index.php
@@ -305,7 +305,7 @@ try {
                     const priceText = offer.querySelector(".prix span")?.textContent.replace("€", "").trim();
                     const price = parseFloat(priceText) || 0;
                     const isAvailable = offer.querySelector(".ouverture-offre")?.textContent.trim() === "Ouvert";
-                    const etoiles = offer.querySelector(".etoiles")?.textContent.trim();
+                    const etoiles = offer.querySelector(".etoile");
                     console.log(etoiles);
                     const note = etoiles.length;
                     console.log('note' + note);
