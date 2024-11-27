07de80feb65611341e2328741f4d6ed0245ba0f9
DardSolf <eletaillandier@gmail.com>
2024-11-27 15:31:28 +0100

    test

--- html/test_nav/index.php
+++ html/test_nav/index.php
@@ -39,7 +39,7 @@ try {
                 }
             ?>
             <script>
-                elementList = parentNode.querySelectorAll('div[option]');
+                elementList = document.querySelectorAll('div[option]');
 
                 elementList.forEach(element => {
                     element.addEventListener("click", (event) => {
