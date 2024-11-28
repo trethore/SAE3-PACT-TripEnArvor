19250fd39aac76a0cf9b5ba48f39607d9dd9dd84
DardSolf <eletaillandier@gmail.com>
2024-11-28 08:44:52 +0100

    test

--- html/test_nav/index.php
+++ html/test_nav/index.php
@@ -1,110 +1,94 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/php/connect_params.php');
-
-try {
-    $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
-    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $dbh->prepare("SET SCHEMA 'sae';")->execute();
-    $stmt = $dbh->prepare('SELECT titre, id_offre FROM sae._offre');
-    $stmt->execute();
-    $offres = $stmt->fetchAll(); // Récupère uniquement la colonne "titre"
-    $dbh = null;
-} catch (PDOException $e) {
-    echo "Erreur lors de la récupération des titres : " . $e->getMessage();
-}
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/style/styleguide.css" />
     <link rel="stylesheet" href="/style/styles.css">
     <link rel="stylesheet" href="/style/style_HFF.css">
-    <link rel="stylesheet" href="/style/style_navPhone.css"/>
+    <link rel="stylesheet" href="/style/style_navPhone.css" />
     <title>Liste de vos offres</title>
 </head>
+
 <body>
     <header>
         <img class="logo" src="/images/universel/logo/Logo_blanc.png" />
         <div class="text-wrapper-17"><a href="/front/consulter-offres">PACT Pro</a></div>
         <div class="search-box">
-            <button class="btn-search"><img class="cherchero" src="/images/universel/icones/chercher.png" /></button>
-            <input type="text" list="cont" class="input-search" placeholder="Taper votre recherche...">
+            <button class="btn-search">
+                <img class="cherchero" src="/images/universel/icones/chercher.png" />
+            </button>
+            <input type="text" list="cont" class="input-search" placeholder="Taper votre recherche..." />
             <datalist id="cont">
-                <?php 
-                foreach ($offres as $offre) { // Parcourt les titres récupérés
-                   ?><option value="<?php echo $offre['titre'] ?>" id ="<?php echo $offre['id_offre'] ?>" ><?php echo $offre['titre'] ?></option><?php
-                }
-            ?>
-            <script>
-                elementList = document.querySelectorAll('div[option]');
-                console.log(elementList);
-
-                elementList.forEach(element => {
-                    element.addEventListener("click", (event) => {
-                        // TODO: set to front pls my dear
-                        console.log("go");
-                        window.location.href = "/back/consulter-offre/index.php?id=" + element.id; 
-                    });
-                });
-
-            </script>
+                <?php foreach ($offres as $offre): ?>
+                    <option value="<?php echo htmlspecialchars($offre['titre']); ?>" data-id="<?php echo $offre['id_offre']; ?>">
+                        <?php echo htmlspecialchars($offre['titre']); ?>
+                    </option>
+                <?php endforeach; ?>
             </datalist>
         </div>
-        <a href="/back/liste-back"><img class="ICON-accueil" src="/images/universel/icones/icon_accueil.png" /></a>
-        <a href="/back/se-connecter"><img class="ICON-utilisateur" src="/images/universel/icones/icon_utilisateur.png" /></a>
+        <a href="/back/liste-back">
+            <img class="ICON-accueil" src="/images/universel/icones/icon_accueil.png" />
+        </a>
+        <a href="/back/se-connecter">
+            <img class="ICON-utilisateur" src="/images/universel/icones/icon_utilisateur.png" />
+        </a>
     </header>
-    <main>
-    </main>
+
+    <main></main>
+
     <footer>
         <div class="footer-top">
-        <div class="footer-top-left">
-            <span class="footer-subtitle">P.A.C.T</span>
-            <span class="footer-title">TripEnArmor</span>
-        </div>
-        <div class="footer-top-right">
-            <span class="footer-connect">Restons connectés !</span>
-            <div class="social-icons">
-            <a href="https://x.com/?locale=fr">
-                <div class="social-icon" style="background-image: url('/images/universel/icones/x.png');"></div>
-            </a>
-            <a href="https://www.facebook.com/?locale=fr_FR">
-                <div class="social-icon" style="background-image: url('/images/universel/icones/facebook.png');"></div>
-            </a>
-            <a href="https://www.youtube.com/">
-                <div class="social-icon" style="background-image: url('/images/universel/icones/youtube.png');"></div>
-            </a>
-            <a href="https://www.instagram.com/">
-                <div class="social-icon" style="background-image: url('/images/universel/icones/instagram.png');"></div>
-            </a>
+            <div class="footer-top-left">
+                <span class="footer-subtitle">P.A.C.T</span>
+                <span class="footer-title">TripEnArmor</span>
+            </div>
+            <div class="footer-top-right">
+                <span class="footer-connect">Restons connectés !</span>
+                <div class="social-icons">
+                    <a href="https://x.com/?locale=fr">
+                        <div class="social-icon" style="background-image: url('/images/universel/icones/x.png');"></div>
+                    </a>
+                    <a href="https://www.facebook.com/?locale=fr_FR">
+                        <div class="social-icon" style="background-image: url('/images/universel/icones/facebook.png');"></div>
+                    </a>
+                    <a href="https://www.youtube.com/">
+                        <div class="social-icon" style="background-image: url('/images/universel/icones/youtube.png');"></div>
+                    </a>
+                    <a href="https://www.instagram.com/">
+                        <div class="social-icon" style="background-image: url('/images/universel/icones/instagram.png');"></div>
+                    </a>
+                </div>
             </div>
         </div>
-
-
-        <!-- Barre en bas du footer incluse ici -->
-
-        </div>
         <div class="footer-bottom">
-        Politique de confidentialité - Politique RGPD - <a href="mention_legal.html">Mentions légales</a> - Plan du site -
-        Conditions générales - ©
-        Redden's, Inc.
+            Politique de confidentialité - Politique RGPD - 
+            <a href="mention_legal.html">Mentions légales</a> - Plan du site -
+            Conditions générales - © Redden's, Inc.
         </div>
     </footer>
 
-    
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            const inputSearch = document.querySelector(".input-search");
+            const datalist = document.querySelector("#cont");
 
+            inputSearch.addEventListener("change", () => {
+                const selectedOption = Array.from(datalist.options).find(
+                    option => option.value === inputSearch.value
+                );
+
+                if (selectedOption) {
+                    const idOffre = selectedOption.getAttribute("data-id");
+                    if (idOffre) {
+                        window.location.href = `/back/consulter-offre/index.php?id=${idOffre}`;
+                    } else {
+                        console.log("Aucune correspondance trouvée.");
+                    }
+                }
+            });
+        });
+    </script>
 </body>
-
-    <div class="telephone-nav">
-        <div class="bg"></div>
-        <div class="nav-content">
-        <div class = "btOn">
-            <img src="/images/frontOffice/icones/accueil.png">
-            </div>
-            <img src="/images/frontOffice/icones/chercher.png">
-            <img src="/images/frontOffice/icones/utilisateur.png">
-        </div>
-    </div>
 </html>
