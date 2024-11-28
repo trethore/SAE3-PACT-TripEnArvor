b66bc39473e469e4d5392299dff5bb2ec25c26cb
StanislasROLLAND <stanislasrolland05@gmail.com>
2024-11-28 10:00:28 +0100

    Back to back

--- html/front/consulter-offre/index.php
+++ html/front/consulter-offre/index.php
@@ -133,8 +133,8 @@ try {
 <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="/style/styleguide.css"/>
-    <link rel="stylesheet" href="/style/styleHFB.css"/>
-    <link rel="stylesheet" href="/style/style-details-offre-pro.css"/>
+    <link rel="stylesheet" href="/style/styleHFF.css"/>
+    <link rel="stylesheet" href="/style/style-details-offre-visiteur.css"/>
     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Seymour+One&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=SeoulNamsan&display=swap" rel="stylesheet">
