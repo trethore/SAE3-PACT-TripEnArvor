6c973eb7567f1d059a5543041cc975d2cbde4b1a
capoupon <camille.poupon@etudiant.univ-rennes.fr>
2024-12-02 17:33:17 +0100

    test js afficher type

--- html/back/creer-offre/index.php
+++ html/back/creer-offre/index.php
@@ -716,12 +716,12 @@
             const isIdProPublique = "<?php echo json_encode($isIdProPublique) ?>";
             console.log(isIdProPublique);
 
-            // if(isIdProPublique){
-            //     console.log("l'id est publique");
-            //      document.getElementById("divtype").style.display = 'none';
-            //      document.getElementById("labeltype").style.display = 'none';
-            //      console.log("le type est caché");
-            // }
+            if(isIdProPublique){
+                console.log("l'id est publique");
+                 document.getElementById("divtype").style.display = 'none';
+                 document.getElementById("labeltype").style.display = 'none';
+                 console.log("le type est caché");
+            }
 
 
 
