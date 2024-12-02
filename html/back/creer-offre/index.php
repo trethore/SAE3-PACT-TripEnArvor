9a8f4081be4b32e42fe2cb1703f44f318776825e
capoupon <camille.poupon@etudiant.univ-rennes.fr>
2024-12-02 17:24:01 +0100

    fix ?

--- html/back/creer-offre/index.php
+++ html/back/creer-offre/index.php
@@ -587,7 +587,7 @@
                     $stmt->execute([$titre, $resume, $ville, intval($age), intval($nbattraction), $fichier_img, $id_compte, $tarif_min, $type]);
 
                     //INSERTION IMAGE DANS _OFFRE_CONTIENT_IMAGE
-                    $requete_offre_contient_image = 'INSERT INTO _offre_contient_image(id_offre, id_image) VALUES (?, ?)';
+                    $requete_plan_offre = 'INSERT INTO _offre_contient_image(id_offre, id_image) VALUES (?, ?)';
                     $stmt_plan_offre = $dbh->prepare($requete_plan_offre);
                     $stmt_plan_offre->execute([$id_offre, $fichier_plan]);
 
@@ -648,9 +648,9 @@
                     }
             
                     //INSERTION IMAGE DANS _OFFRE_CONTIENT_IMAGE
-                    $requete_offre_contient_image = 'INSERT INTO _offre_contient_image(id_offre, id_image) VALUES (?, ?)';
-                    $stmt_plan_image = $dbh->prepare($requete_plan_offre);
-                    $stmt_plan_image->execute([$id_offre, $fichier_carte]);
+                    $requete_carte_offre = 'INSERT INTO _offre_contient_image(id_offre, id_image) VALUES (?, ?)';
+                    $stmt_carte_image = $dbh->prepare($requete_carte_offre);
+                    $stmt_carte_image->execute([$id_offre, $fichier_carte]);
 
                     $id_offre = $stmt->fetch(PDO::FETCH_ASSOC)['id_offre'];
                     break;
