967a79283f11fae05f2f6595a7f91a737a1debdb
capoupon <camille.poupon@etudiant.univ-rennes.fr>
2024-12-02 17:44:54 +0100

    test js

--- html/back/creer-offre/index.php
+++ html/back/creer-offre/index.php
@@ -716,7 +716,7 @@
             const isIdProPublique = "<?php echo json_encode($isIdProPublique) ?>";
             console.log(isIdProPublique);
 
-            if(isIdProPublique){
+            if(isIdProPubliquen === true){
                 console.log("l'id est publique");
                  document.getElementById("divtype").style.display = 'none';
                  document.getElementById("labeltype").style.display = 'none';
