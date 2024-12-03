441766c1c363eba8d5c7785d420768bffa7f6bef
DardSolf <151910992+DardSolf@users.noreply.github.com>
2024-12-03 14:40:23 +0100

    Update file_paths.php

--- file_paths.php
+++ file_paths.php
@@ -1,1 +1,14 @@
+<?php
+// Routes pour le back-office
+define('LISTE_BACK', BASE_URL . '/back/liste-back');
+define('MON_COMPTE_BACK', BASE_URL . '/back/mon-compte');
+define('MES_FACTURES', BASE_URL . '/back/mes-factures');
 
+// Routes pour le front-office
+define('CONSULTER_OFFRES', BASE_URL . '/front/consulter-offres');
+define('MON_COMPTE_FRONT', BASE_URL . '/front/mon-compte');
+
+// Routes générales
+define('SE_CONNECTER', BASE_URL . '/se-connecter');
+
+?>
