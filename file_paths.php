4c67b4300cb1816d0aea301c14180ab5c7ce5ab7
DardSolf <151910992+DardSolf@users.noreply.github.com>
2024-12-03 14:48:54 +0100

    Update file_paths.php

--- file_paths.php
+++ file_paths.php
@@ -1,14 +1,21 @@
 <?php
+
 // Routes pour le back-office
-define('LISTE_BACK', BASE_URL . '/back/liste-back');
-define('MON_COMPTE_BACK', BASE_URL . '/back/mon-compte');
-define('MES_FACTURES', BASE_URL . '/back/mes-factures');
+const BACK_CONSULTER_OFFRE = BASE_URL . '/back/consulter-offre';
+const BACK_CREER_OFFRE = BASE_URL . '/back/creer-offre';
+const BACK_LISTE_BACK = BASE_URL . '/back/liste-back';
+const BACK_MES_FACTURES = BASE_URL . '/back/mes-factures';
+const BACK_MODIFIER_COMPTE = BASE_URL . '/back/modifier-compte';
+const BACK_MODIFIER_OFFRE = BASE_URL . '/back/modifier-offre';
+const BACK_MON_COMPTE = BASE_URL . '/back/mon-compte';
 
 // Routes pour le front-office
-define('CONSULTER_OFFRES', BASE_URL . '/front/consulter-offres');
-define('MON_COMPTE_FRONT', BASE_URL . '/front/mon-compte');
+const FRONT_CONSULTER_OFFRE = BASE_URL . '/front/consulter-offre';
+const FRONT_CONSULTER_OFFRES = BASE_URL . '/front/consulter-offres';
+const FRONT_MON_COMPTE = BASE_URL . '/front/mon-compte';
 
-// Routes générales
-define('SE_CONNECTER', BASE_URL . '/se-connecter');
+// Route générale
+const CREER_COMPTE = BASE_URL . '/creer-compte';
+const SE_CONNECTER = BASE_URL . '/se-connecter';
 
 ?>
