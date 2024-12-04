704502e4f171ad0adedb4b73fe1ffe4ea23c0f4a
pik <pikdev.gopi@gmail.com>
2024-12-04 10:41:53 +0100

    test

--- html/front/consulter-offres/index.php
+++ html/front/consulter-offres/index.php
@@ -28,6 +28,9 @@ try {
 
     foreach ($offres as &$offre) {
         $offre['prix'] = getPrixPlusPetit($offre['id_offre']);
+        echo '<pre>';
+        echo $offre['prix'];
+        echo '</pre>';
     }
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
