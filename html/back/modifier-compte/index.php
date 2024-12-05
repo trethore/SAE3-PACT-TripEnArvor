244cc3dde1001a3877c308777227536904a96a65
trethore <titou.rethore@gmail.com>
2024-12-05 09:43:28 +0100

    file_paths

--- html/back/modifier-compte/index.php
+++ html/back/modifier-compte/index.php
@@ -1,6 +1,6 @@
 <?php 
 ob_start();
-require_once($_SERVER['DOCUMENT_ROOT'] . "/utils/file_paths-utils.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/file_paths-utils.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . SESSION_UTILS);
 require_once($_SERVER['DOCUMENT_ROOT'] . CONNECT_PARAMS);
 require_once($_SERVER['DOCUMENT_ROOT'] . COMPTE_UTILS);
