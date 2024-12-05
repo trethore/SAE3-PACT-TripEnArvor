527c63e53baf7b1174d8b3cdb64d3e89e2453e98
capoupon <camille.poupon@etudiant.univ-rennes.fr>
2024-12-05 10:09:41 +0100

    test

--- html/back/modifier-offre/index.php
+++ html/back/modifier-offre/index.php
@@ -398,7 +398,7 @@ try {
                         </tr>
                     </table> -->
                     <div class="bt_cree">
-                        <button class="valider" type="submit" value="Modifier l'offre" onclick="location.href='../../back/modifier-offre/index.php?id=<?php echo $id_offre_cible ?>'" > </button>
+                        <input class="valider" type="submit" value="Modifier l'offre">
 
                         <a href="#" id="back-to-top">
                             <img src="/images/backOffice/icones/fleche-vers-le-haut.png" alt="Retour en haut" width="50"
