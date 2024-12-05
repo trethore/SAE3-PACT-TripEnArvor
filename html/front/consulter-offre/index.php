dee7e0288450300366da94cde3e04563e828ddeb
StanislasROLLAND <stanislasrolland05@gmail.com>
2024-12-05 14:33:17 +0100

    Test

--- html/front/consulter-offre/index.php
+++ html/front/consulter-offre/index.php
@@ -593,8 +593,8 @@ try {
                         </div>
                         <?php $passage = explode(' ', $datePassage[$compteur]['date']);
                         $datePass = explode('-', $passage[0]); ?>
-                        <p>Visité le : <?php echo htmlentities($datePass[2] . "/" . $datePass[1] . "/" . $datePass[0]); ?> Contexte : <?php echo htmlentities($a['contexte_visite']); ?></p>
-                        <p><?php echo htmlentities(html_entity_decode($a['commentaire'])); ?></p>
+                        <p>Visité le : <?php echo htmlentities($datePass[2] . "/" . $datePass[1] . "/" . $datePass[0]); ?> Contexte : <?php echo htmlentities($n['contexte_visite']); ?></p>
+                        <p><?php echo htmlentities(html_entity_decode($n['commentaire'])); ?></p>
                         <!-- <div class="display-ligne-espace">
                             <p class="transparent">.</p>
                             <div class="display-notation">
