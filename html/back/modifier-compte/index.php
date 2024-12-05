074bb9899c5956db678f4117b659a9de0b46004e
MatheoPf <matheo.pfranger@gmail.com>
2024-12-05 09:31:13 +0100

    test

--- html/back/modifier-compte/index.php
+++ html/back/modifier-compte/index.php
@@ -9,8 +9,6 @@ require_once($_SERVER['DOCUMENT_ROOT'] . COMPTE_UTILS);
 require_once($_SERVER['DOCUMENT_ROOT'] . SITE_UTILS);
 require_once($_SERVER['DOCUMENT_ROOT'] . AUTH_UTILS);
 
-print_r($_SESSION);
-
 try {
     $conn = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
 } catch (PDOException $e) {
