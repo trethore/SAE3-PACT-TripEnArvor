17c531f1d43fd7bab159e30f5053778a1ef9bef0
MatheoPf <matheo.pfranger@gmail.com>
2025-01-08 14:46:20 +0100

    css unique

--- html/back/modifier-compte/index.php
+++ html/back/modifier-compte/index.php
@@ -45,7 +45,6 @@ switch ($typeCompte) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style/style_backCompte.css">
     <link rel="stylesheet" href="/style/style.css">
 
     <title>Modifier mon compte</title>
--- html/front/mon-compte/index.php
+++ html/front/mon-compte/index.php
@@ -35,9 +35,7 @@ $reqAdresse = "SELECT num_et_nom_de_voie, complement_adresse, code_postal, ville
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style/style_frontCompte.css">
-    <link rel="stylesheet" href="/style/style_HFF.css">
-    <link rel="stylesheet" href="/style/styleguide.css">
+    <link rel="stylesheet" href="/style/style.css">
     <title>Mon compte</title>
     <link rel="icon" type="image/jpeg" href="/images/universel/logo/Logo_icone.jpg">
 </head>
