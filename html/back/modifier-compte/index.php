bcc9997fe0589823aebc02b739720281dd8ba5f0
MatheoPf <matheo.pfranger@gmail.com>
2025-01-08 14:59:26 +0100

    tes css unique

--- html/back/modifier-compte/index.php
+++ html/back/modifier-compte/index.php
@@ -46,7 +46,6 @@ switch ($typeCompte) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/style/style.css">
-
     <title>Modifier mon compte</title>
     <link rel="icon" type="image/jpeg" href="/images/universel/logo/Logo_icone.jpg">
 
