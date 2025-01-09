0cb4442a15194b7ef717c67c636d92e60de5f130
pik <pikdev.gopi@gmail.com>
2025-01-09 10:55:16 +0100

    fix note acceuil

--- html/front/accueil/index.php
+++ html/front/accueil/index.php
@@ -217,18 +217,18 @@ try {
             if (note === 0) {
                 starsHTML = "Pas d'avis disponibles.";
             } else {
-                $demiEtoile = ($note - $etoilesPleines) == 0.5 ? 1 : 0;
-                $etoilesVides = 5 - $etoilesPleines - $demiEtoile;
+                demiEtoile = (note - etoilesPleines) == 0.5 ? 1 : 0;
+                etoilesVides = 5 - etoilesPleines - demiEtoile;
 
-                for ($i = 0; $i < $etoilesPleines; $i++) {
+                for (i = 0; i < $etoilesPleines; i++) {
                     starsHTML += '<img src="/images/frontOffice/etoile-pleine.png" alt="Star pleine">';
                 }
 
-                if ($demiEtoile) {
+                if (demiEtoile) {
                     starsHTML += '<img src="/images/frontOffice/etoile-moitie.png" alt="Star moitie">';
                 }
                     
-                for ($i = 0; $i < $etoilesVides; $i++) {
+                for (i = 0; $i < etoilesVides; $i++) {
                     starsHTML += '<img src="/images/frontOffice/etoile-vide.png" alt="Star vide">';
                 }
             }
