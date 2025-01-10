57f6ec4fca6b98fd50a5459784c6f4749826a36d
DardSolf <eletaillandier@gmail.com>
2025-01-10 10:27:01 +0100

    test22

--- html/front/modifier-compte/index.php
+++ html/front/modifier-compte/index.php
@@ -33,6 +33,7 @@ $reqCompte = "SELECT * from sae.compte_membre
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width"/>
     <link rel="stylesheet" href="/style/style.css">
     <title>Modifier mon compte</title>
     <link rel="icon" type="image/jpeg" href="/images/universel/logo/Logo_icone.jpg">
