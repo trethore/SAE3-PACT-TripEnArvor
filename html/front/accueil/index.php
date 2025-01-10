9050d59393b90033ea2e49bde65a74e6558fd696
jocooper <jocooper2018@gmail.com>
2025-01-10 10:18:43 +0100

    Liens cgu + politique de confidentialité du footer pages front

--- html/front/accueil/index.php
+++ html/front/accueil/index.php
@@ -196,8 +196,8 @@ try {
 
         </div>
         <div class="footer-bottom">
-        Politique de confidentialité - Politique RGPD - <a href="mention_legal.html">Mentions légales</a> - Plan du site -
-        Conditions générales - ©
+        <a href="/confidentialité/" target="_blank">Politique de confidentialité</a> - Politique RGPD - <a href="mention_legal.html">Mentions légales</a> - Plan du site -
+        <a href="/cgu/" target="_blank">Conditions générales</a> - ©
         Redden's, Inc.
         </div>
     </footer>
