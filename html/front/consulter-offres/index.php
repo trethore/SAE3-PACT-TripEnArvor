10a1515aade7e82f54d1f04a7ee00c7b4752452b
DardSolf <eletaillandier@gmail.com>
2025-01-10 14:27:13 +0100

    test38

--- html/front/consulter-offres/index.php
+++ html/front/consulter-offres/index.php
@@ -544,11 +544,8 @@ try {
     <div class="bg"></div>
     <div class="nav-content">
         <a href="/front/accueil"><img src="/images/frontOffice/icones/accueil.png"></a>
-        <a href="/front/consulter-offres"><img src="/images/frontOffice/icones/chercher.png"></a>
-        <a href="/front/mon-compte">
-        <div class = "btOn">
-            <img src="/images/frontOffice/icones/utilisateur.png">
-        </div></a>
+        <a href="/front/consulter-offres"><div class = "btOn"><img src="/images/frontOffice/icones/chercher.png"></div></a>
+        <a href="/front/mon-compte"><img src="/images/frontOffice/icones/utilisateur.png"></a>
     </div>
 </div>
 
