c4f173812ffde2beb679feb32ced6088ef477db3
capoupon <camille.poupon@etudiant.univ-rennes.fr>
2025-01-10 09:56:46 +0100

    fix date normalement

--- html/back/modifier-offre/index.php
+++ html/back/modifier-offre/index.php
@@ -194,15 +194,15 @@ try {
     print_r($offre_bonne_cat);
 
     if (($categorie == 'spectacle')) {
-        $date_evenement = getDateSpectacle($id_offre_cible);
+        $date_evenement = getDateSpectacle($id_offre_cible)[0];
     }elseif ($categorie == 'visite') {
-        $date_evenement = getDateVisite($id_offre_cible);
+        $date_evenement = getDateVisite($id_offre_cible)[0];
     }else {
         $date_evenement = null; // Gestion par défaut si aucune catégorie ne correspond
     }
 
     print_r (getDateSpectacle($id_offre_cible));
-    print_r (getDateSpectacle($id_offre_cible));
+    print_r (getDateVisite($id_offre_cible));
     print_r( $date_evenement);
     
 
