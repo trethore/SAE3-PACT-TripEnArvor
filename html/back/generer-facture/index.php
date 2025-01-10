e9d62ce854701c32c54faf152f12acfba1aa5d10
MatheoPf <matheo.pfranger@gmail.com>
2025-01-10 12:07:47 +0100

    table

--- html/back/generer-facture/index.php
+++ html/back/generer-facture/index.php
@@ -28,32 +28,32 @@
             <thead>
                 <tr>
                     <th>Description</th>
+                    <th>Date</th>
                     <th>Qté</th>
+                    <th>% TVA</th>
                     <th>Prix HT</th>
                     <th>Prix Unitaire</th>
                     <th>Prix TTC</th>
-                    <th>% TVA</th>
-                    <th>Date</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td>Option relief</td>
+                    <td>15/04/2024</td>
                     <td>2</td>
-                    <td>6.50€</td>
+                    <td>20%</td>
+                    <td>5.10€</td>
                     <td>6.50€</td>
                     <td>22.30€</td>
-                    <td>20%</td>
-                    <td>15/04/2024</td>
                 </tr>
                 <tr>
                     <td>Offre premium</td>
+                    <td>17/04/2024</td>
                     <td>1</td>
+                    <td>20%</td>
                     <td>24.75€</td>
                     <td>24.75€</td>
                     <td>5.10€</td>
-                    <td>20%</td>
-                    <td>17/04/2024</td>
                 </tr>
             </tbody>
         </table>
