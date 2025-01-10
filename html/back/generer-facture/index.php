660c04a8924d9966d0f3913da0f39d3a7ffcf172
MatheoPf <matheo.pfranger@gmail.com>
2025-01-10 10:55:03 +0100

    html css

--- html/back/generer-facture/index.php
+++ html/back/generer-facture/index.php
@@ -7,74 +7,71 @@
     <link rel="stylesheet" href="/style/style.css">
 </head>
 <body class="genFacture">
-    <div class="invoice">
-        <section>
-            <article class="issued-to">
-                <h3>Délivré à</h3>
-                <p>
-                    {Nom} {Prénom}<br>
-                    {Dénomination sociale}<br>
-                    mail@example.com
-                </p>
-            </article>
-            <article>
-                <h3>Numéro de facture</h3>
-                <h3>#012345</h3>
-                <p>{jj/mm/aaaa}</p>
-            </article>
-        </section>
-        <section class="invoice-details">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Description</th>
-                        <th>Qté</th>
-                        <th>Prix HT</th>
-                        <th>Prix Unitaire</th>
-                        <th>Prix TTC</th>
-                        <th>% TVA</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Option relief</td>
-                        <td>2</td>
-                        <td>6.50€</td>
-                        <td>6.50€</td>
-                        <td>22.30€</td>
-                        <td>20%</td>
-                        <td>15/04/2024</td>
-                    </tr>
-                    <tr>
-                        <td>Offre premium</td>
-                        <td>1</td>
-                        <td>24.75€</td>
-                        <td>24.75€</td>
-                        <td>5.10€</td>
-                        <td>20%</td>
-                        <td>17/04/2024</td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>
-        <article class="totals">
-            <hr>
-            <p>Total HT: 31.25€</p>
-            <p>Total TVA: 10€</p>
-            <p>Total TTC: 34.38€</p>
-            <hr>
-        </article>
-
-        <article class="payment-terms">
-            <h3>Conditions et modalités de paiement</h3>
-            <p>Le paiement est dû dans les 15 jours</p>
+    <section>
+        <article class="delivre">
+            <h3>Délivré à</h3>
             <p>
-                Nom Banque<br>
-                Nom du compte: Nom<br>
-                Numéro de compte : 123-456-7890
+                {Nom} {Prénom}<br>
+                {Dénomination sociale}<br>
+                mail@example.com
             </p>
         </article>
-    </div>
+        <article>
+            <h3>Numéro de facture</h3>
+            <h3>#012345</h3>
+            <p>{jj/mm/aaaa}</p>
+        </article>
+    </section>
+    <section class="facture-details">
+        <table>
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Qté</th>
+                    <th>Prix HT</th>
+                    <th>Prix Unitaire</th>
+                    <th>Prix TTC</th>
+                    <th>% TVA</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Option relief</td>
+                    <td>2</td>
+                    <td>6.50€</td>
+                    <td>6.50€</td>
+                    <td>22.30€</td>
+                    <td>20%</td>
+                    <td>15/04/2024</td>
+                </tr>
+                <tr>
+                    <td>Offre premium</td>
+                    <td>1</td>
+                    <td>24.75€</td>
+                    <td>24.75€</td>
+                    <td>5.10€</td>
+                    <td>20%</td>
+                    <td>17/04/2024</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+    <article class="totals">
+        <hr>
+        <p>Total HT: 31.25€</p>
+        <p>Total TVA: 10€</p>
+        <p>Total TTC: 34.38€</p>
+        <hr>
+    </article>
+    <article class="payment-terms">
+        <h3>Conditions et modalités de paiement</h3>
+        <p>Le paiement est dû dans les 15 jours</p>
+        <p>
+            Nom Banque<br>
+            Nom du compte: Nom<br>
+            Numéro de compte : 123-456-7890
+        </p>
+    </article>
 </body>
 </html>
