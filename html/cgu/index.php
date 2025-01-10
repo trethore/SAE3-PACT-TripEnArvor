5f2a0e4dbd30ded23489c82ba61537ce8871d159
jocooper <jocooper2018@gmail.com>
2025-01-10 09:55:27 +0100

    Créations des page des cgu et de la politique de confidentialité & màj des liens vers ces page dans créer compte.

---
+++ html/cgu/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conditions générales d'utilisations</title>
+</head>
+<body>
+    <main>
+        <h1>Conditions générales d'utilisations</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam esse neque nisi eius aperiam quas fugit beatae quam deserunt possimus explicabo aliquam, ea, quis laudantium doloribus, amet nemo voluptatem sequi.
+        </p>
+    </main>
+</body>
+</html>
---
+++ html/confidentialité/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Politique de confidentialité</title>
+</head>
+<body>
+    <main>
+        <h1>Politique de confidentialité</h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt odio consequatur, laudantium neque nihil similique. Ducimus dolore, autem, blanditiis ex libero porro rem officiis minima laboriosam totam, eos accusamus tempore.
+        </p>
+    </main>
+</body>
+</html>
