4a3a688da7942edfb85c875efdbb04e0d892896d
trethore <titou.rethore@gmail.com>
2025-01-13 09:57:32 +0100

    update main.c

--- tchatator/src/serveur/main.c
+++ tchatator/src/serveur/main.c
@@ -1,7 +1,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <libpq-fe.h>
+
 int main() {
+
     printf("Serveur Loaded !");
     return EXIT_SUCCESS;
 }
