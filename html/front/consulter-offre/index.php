bbc393203721cc898d2a046fcc0a405e635ac4bf
pik <pikdev.gopi@gmail.com>
2025-01-13 15:09:41 +0100

    barbafix

--- html/front/consulter-offre/index.php
+++ html/front/consulter-offre/index.php
@@ -2,6 +2,7 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/file_paths-utils.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . CONNECT_PARAMS);
 require_once($_SERVER['DOCUMENT_ROOT'] . OFFRES_UTILS);
+require_once($_SERVER['DOCUMENT_ROOT'] . SESSION_UTILS);
 
 date_default_timezone_set('Europe/Paris');
 
