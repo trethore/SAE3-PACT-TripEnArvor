b3fadcd5d135c2c55dd7944940d576913d7c2d7a
jocooper <jocooper2018@gmail.com>
2025-01-15 16:04:20 +0100

    Refonte du carousel sur la page d’accueil.

--- html/front/accueil/index.php
+++ html/front/accueil/index.php
@@ -23,6 +23,7 @@ if (!isset($_SESSION)) {
     <link rel="stylesheet" href="/style/style.css">
     <title>Accueil</title>
     <link rel="icon" type="image/jpeg" href="/images/universel/logo/Logo_icone.jpg">
+    <script src="/scripts/carousel.js"></script>
 </head>
 
 <body class="front accueil">
@@ -120,7 +121,7 @@ if (!isset($_SESSION)) {
         ?>
 
         <section>
-            <div class="carousel">
+            <!-- <div class="carousel">
                 <div class="carousel-images">
                     <?php foreach ($ids as $offre) {
                         $id = $offre["id_offre"];
@@ -140,7 +141,59 @@ if (!isset($_SESSION)) {
                     </div>
                 </div>
                 <p class="titre" id="carousel-titre"></p>
+            </div> -->
+
+        <div class="carousel">
+            <div class="carousel-slides">
+<?php
+foreach ($ids as $offre) {
+?>
+                <a href="/front/consulter-offre/index.php?id=<?php echo($offre['id_offre']); ?>" class="slide">
+                    <img src="/images/universel/photos/<?php echo htmlentities(getFirstIMG($offre["id_offre"])) ?>">
+                    <div>
+                        <span>Azerty</span>
+                        <div>
+<?php
+    $noteMoyenne = getNoteMoyenne($offre["id_offre"]);
+
+    if ($noteMoyenne !== null) {
+
+        $etoilesPleines = floor($noteMoyenne);
+        $demiEtoile = ($noteMoyenne - $etoilesPleines) == 0.5 ? 1 : 0;
+        $etoilesVides = 5 - $etoilesPleines - $demiEtoile;
+        
+        for ($i = 0; $i < $etoilesPleines; $i++) {
+?>
+                            <img class="etoile" src="/images/frontOffice/etoile-pleine.png">
+<?php 
+        }
+        if ($demiEtoile) {
+?>
+                            <img class="etoile" src="/images/frontOffice/etoile-moitie.png">
+<?php
+        }
+        for ($i = 0; $i < $etoilesVides; $i++) {
+?>
+                            <img class="etoile" src="/images/frontOffice/etoile-vide.png">
+<?php
+        }
+
+    } else {
+?>
+                            <span>Pas d'avis</span>
+<?php
+    }
+?>
+                        </div>
+                    </div>
+                </a>
+<?php
+}
+?>
             </div>
+            <button type="button" class="prev-slide"><img src="/images/universel/icones/fleche-gauche.png" alt="←"></button>
+            <button type="button" class="next-slide"><img src="/images/universel/icones/fleche-droite.png" alt="→"></button>
+        </div>
         </section>
 
         <h1><a href="/front/consulter-offres">Découvrir la Liste des Offres Disponibles</a></h1>
@@ -148,7 +201,7 @@ if (!isset($_SESSION)) {
         <h2>Nouveautés</h2>
 
         <section>
-            <div class="carousel">
+            <!-- <div class="carousel">
                 <div class="carousel-images">
                     <?php foreach ($ids_nouv as $offre_nouv) {
                         $id = $offre_nouv["id_offre"];
@@ -169,13 +222,65 @@ if (!isset($_SESSION)) {
                     </div>
                 </div>
                 <p class="titre" id="carousel-titre"></p>
+            </div> -->
+            <div class="carousel">
+            <div class="carousel-slides">
+<?php
+foreach ($ids_nouv as $offre) {
+?>
+                <a href="/front/consulter-offre/index.php?id=<?php echo($offre['id_offre']); ?>" class="slide">
+                    <img src="/images/universel/photos/<?php echo htmlentities(getFirstIMG($offre["id_offre"])) ?>">
+                    <div>
+                        <span>Azerty</span>
+                        <div>
+<?php
+    $noteMoyenne = getNoteMoyenne($offre["id_offre"]);
+
+    if ($noteMoyenne !== null) {
+
+        $etoilesPleines = floor($noteMoyenne);
+        $demiEtoile = ($noteMoyenne - $etoilesPleines) == 0.5 ? 1 : 0;
+        $etoilesVides = 5 - $etoilesPleines - $demiEtoile;
+        
+        for ($i = 0; $i < $etoilesPleines; $i++) {
+?>
+                            <img class="etoile" src="/images/frontOffice/etoile-pleine.png">
+<?php 
+        }
+        if ($demiEtoile) {
+?>
+                            <img class="etoile" src="/images/frontOffice/etoile-moitie.png">
+<?php
+        }
+        for ($i = 0; $i < $etoilesVides; $i++) {
+?>
+                            <img class="etoile" src="/images/frontOffice/etoile-vide.png">
+<?php
+        }
+
+    } else {
+?>
+                            <span>Pas d'avis</span>
+<?php
+    }
+?>
+                        </div>
+                    </div>
+                </a>
+<?php
+}
+?>
             </div>
+            <button type="button" class="prev-slide"><img src="/images/universel/icones/fleche-gauche.png" alt="←"></button>
+            <button type="button" class="next-slide"><img src="/images/universel/icones/fleche-droite.png" alt="→"></button>
+        </div>
+
         </section>
 
         <h2>Consultés Récemment</h2>
 
         <section>
-            <div class="carousel">
+            <!-- <div class="carousel">
                 <div class="carousel-images">
                 <?php foreach ($ids_consulte as $offre_consulte) {
                         $id = $offre_consulte["id_offre"];
@@ -196,7 +301,58 @@ if (!isset($_SESSION)) {
                     </div>
                 </div>
                 <p class="titre" id="carousel-titre"></p>
+            </div> -->
+            <div class="carousel">
+            <div class="carousel-slides">
+<?php
+foreach ($ids_consulte as $offre) {
+?>
+                <a href="/front/consulter-offre/index.php?id=<?php echo($offre['id_offre']); ?>" class="slide">
+                    <img src="/images/universel/photos/<?php echo htmlentities(getFirstIMG($offre["id_offre"])) ?>">
+                    <div>
+                        <span>Azerty</span>
+                        <div>
+<?php
+    $noteMoyenne = getNoteMoyenne($offre["id_offre"]);
+
+    if ($noteMoyenne !== null) {
+
+        $etoilesPleines = floor($noteMoyenne);
+        $demiEtoile = ($noteMoyenne - $etoilesPleines) == 0.5 ? 1 : 0;
+        $etoilesVides = 5 - $etoilesPleines - $demiEtoile;
+        
+        for ($i = 0; $i < $etoilesPleines; $i++) {
+?>
+                            <img class="etoile" src="/images/frontOffice/etoile-pleine.png">
+<?php 
+        }
+        if ($demiEtoile) {
+?>
+                            <img class="etoile" src="/images/frontOffice/etoile-moitie.png">
+<?php
+        }
+        for ($i = 0; $i < $etoilesVides; $i++) {
+?>
+                            <img class="etoile" src="/images/frontOffice/etoile-vide.png">
+<?php
+        }
+
+    } else {
+?>
+                            <span>Pas d'avis</span>
+<?php
+    }
+?>
+                        </div>
+                    </div>
+                </a>
+<?php
+}
+?>
             </div>
+            <button type="button" class="prev-slide"><img src="/images/universel/icones/fleche-gauche.png" alt="←"></button>
+            <button type="button" class="next-slide"><img src="/images/universel/icones/fleche-droite.png" alt="→"></button>
+        </div>
         </section>
     </main>
 
@@ -234,8 +390,8 @@ if (!isset($_SESSION)) {
         </div>
     </footer>
 
-    <script>
-        document.querySelectorAll('.carousel').forEach(carousel => {
+    <!-- <script>
+        /* document.querySelectorAll('.carousel').forEach(carousel => {
             const images = carousel.querySelector('.carousel-images');
             const prevButton = carousel.querySelector('.prev');
             const nextButton = carousel.querySelector('.next');
@@ -287,8 +443,8 @@ if (!isset($_SESSION)) {
             });
 
             updateCarousel();
-        });
-    </script>
+        }); */
+    </script> -->
     <div class="telephone-nav">
         <div class="nav-content">
             <a href="/front/accueil">
