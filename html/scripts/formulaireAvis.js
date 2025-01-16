6d10d7556cb8ff1488e440b2df943ce8c29911be
StanislasROLLAND <stanislasrolland05@gmail.com>
2025-01-16 09:02:49 +0100

    Js

---
+++ html/scripts/formulaireAvis.js
@@ -0,0 +1,13 @@
+const showFormButton = document.getElementById('showFormButton');
+const avisForm = document.getElementById('avisForm');
+const cancelFormButton = document.getElementById('cancelFormButton');
+
+showFormButton.addEventListener('click', () => {
+    avisForm.style.display = 'block'; 
+    showFormButton.style.display = 'none';
+});
+
+cancelFormButton.addEventListener('click', () => {
+    avisForm.style.display = 'none'; 
+    showFormButton.style.display = 'block'; 
+});
