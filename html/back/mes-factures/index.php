1aa8f3efeb95b20e7652c7b06f9161093ddf0a82
MatheoPf <matheo.pfranger@gmail.com>
2025-01-16 21:32:27 +0100

    test

--- html/back/mes-factures/index.php
+++ html/back/mes-factures/index.php
@@ -118,7 +118,7 @@ try {
                 foreach ($factures as $facture) {
                 ?>
                     <li>
-                        <a href="/back/generer-facture/index.php?numero_facture=<?php echo urlencode($facture["numero_facture"]); ?>" target="_blank">Facture N°<?php echo htmlentities($facture["numero_facture"]); ?> - <?php echo htmlentities($facture["titre"]); ?> - <?php echo htmlentities($facture["date_emission"]); ?></a>
+                        <a href="/back/generer-facture/index.php?numero_facture=<?php echo urlencode($facture["numero_facture"]); ?>" target="_blank">Facture N°<?php echo htmlentities($facture["numero_facture"]); ?> - <?php echo htmlentities($facture["titre"]); ?> - <?php echo htmlentities($facture["date"]); ?></a>
                     </li>
                 <?php
                 }
