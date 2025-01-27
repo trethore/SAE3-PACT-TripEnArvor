1beedb7eba18654e654dc19fa8a450e4485d6578
MatheoPf <matheo.pfranger@gmail.com>
2025-01-27 09:44:54 +0100

    création du menu pour le client

--- tchatator/src/client/main.c
+++ tchatator/src/client/main.c
@@ -2,6 +2,83 @@
 #include <stdlib.h>
 #include <stdio.h>
 int main() {
-    printf("Client Loaded !");
+    int port, choixMenu = 0, choixRecep, choixConv, choixModif;
+    char cleAPI[256];
+    printf("Client Loaded !");    
+
+    printf("Veuillez inserez votre port : ");
+    scanf("%d", &port);
+    printf("Veuillez inserez votre cle API : ");
+    scanf("%c", cleAPI);
+
+    do {
+        printf("\n\n");
+        printf("################# MENU #################\n");
+        printf("1. Envoyer un message\n");
+        printf("2. Lire les messages\n");
+        printf("3. Modifier un message\n");
+        printf("\nTapez le chiffre associé à une action : ");
+        scanf("%d", choixMenu);
+
+        switch (choixMenu) {
+        case 1:
+            printf("\n\n");
+            printf("################# ENVOYER UN MESSAGE #################\n");
+            printf("Choisir votre destinataire : ");
+            // Comment choisir le destinataire ?
+            printf("Ecrivez votre message : ");
+            // Comment envoyer un message ?
+            break;
+
+        case 2:
+            printf("\n\n");
+            printf("################# LIRE LES MESSAGE #################\n");
+            printf("1. Voir les messages non lues");
+            printf("2. Afficher une conversation");
+            scanf("%d", choixRecep);
+            switch (choixRecep)
+            {
+            case 1:
+                // Affichage des msgs non lues
+                break;
+            
+            case 2:
+                // Affichage d'une conversation
+                printf("\n\n");
+                printf("################# AFFICHAGE D'UNE CONVERSATION #################\n");
+                printf("1. Conversation avec pro 1");
+                printf("2. Conversation avec pro 1");
+                scanf("%d", choixConv);
+                switch (choixConv)
+                {
+                case 1:
+                    // Affichage de la conversation avec pro 1
+                    break;
+
+                case 2:
+                    // Affichage de la conversation avec pro 2
+                    break;
+                
+                default:
+                    break;
+                }
+                break;
+
+            case 3:
+                printf("\n\n");
+                printf("################# MODIFICATION D'UN MESSAGE #################\n");
+                scanf("%d", choixModif);
+                break;
+
+            default:
+                break;
+            }
+            break;
+        
+        default:
+            break;
+        }
+    } while (choixMenu != -1);
+
     return EXIT_SUCCESS;
 }
