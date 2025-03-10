0c45671344505d1c8e9a0d6ac70531640e06cf42
capoupon <camille.poupon@etudiant.univ-rennes.fr>
2025-03-10 14:49:11 +0100

    test print_r

--- html/back/modifier-offre/index.php
+++ html/back/modifier-offre/index.php
@@ -792,7 +792,7 @@ try {
                 $categorie = $_POST['lacat'];
             }
 
-            print_r($_POST[]);
+            print_r($_POST);
             $tagsSelectionnes = [];
 
             if (!empty($_POST['tag']) && is_array($_POST['tag'])) {
