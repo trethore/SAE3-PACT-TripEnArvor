8483c7727903b3f23d8dd8643ae3095c2bc7f4c4
pik <pikdev.gopi@gmail.com>
2025-03-11 17:21:35 +0100

    ajout du js du toast

--- html/back/liste-back/index.php
+++ html/back/liste-back/index.php
@@ -458,6 +458,32 @@ try {
     </footer>
 
     <script>
+        var toast = document.querySelector(".toast");
+        var btn = document.querySelector(".toast-btn");
+        var close = document.querySelector(".toast-close");
+        var progress = document.querySelector(".progress");
+
+        btn.addEventListener("click", () =>{
+        toast.classList.add("active");
+        progress.classList.add("active");
+
+        setTimeout(() =>{
+            toast.classList.remove("active");
+        }, 5000)
+
+        setTimeout(() =>{
+            progress.classList.remove("active");
+        }, 5300)
+        })
+
+        close.addEventListener("click", () =>{
+        toast.classList.remove("active");
+
+        setTimeout(() =>{
+            progress.classList.remove("active");
+        }, 300)
+        })
+
         document.addEventListener("DOMContentLoaded", () => {
             const h2 = document.querySelector(".filtre-tri h2");
             const fondFiltres = document.querySelector(".fond-filtres");
