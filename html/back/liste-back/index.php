4c9ead028e26a06a7b1669cdd05c88f1cb32012d
pik <pikdev.gopi@gmail.com>
2025-03-12 09:20:13 +0100

    reupdate attribut

--- html/back/liste-back/index.php
+++ html/back/liste-back/index.php
@@ -288,7 +288,7 @@ try {
                     Choix de l'icone pour reconnaitre une offre gratuite, payante ou premium 
                     ------------------------------------------------------------------------>
                     <img src=" <?php
-                    switch ($row["mon_abonnement"]) {
+                    switch ($row["nom_abonnement"]) {
                         case 'gratuit':
                             echo htmlentities("/images/backOffice/icones/gratuit.png");
                             break;
