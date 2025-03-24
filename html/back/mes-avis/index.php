2c5738a04de2dda88490a227b30b6a46e1b27511
capoupon <camille.poupon@etudiant.univ-rennes.fr>
2025-03-24 14:39:15 +0100

    test

--- html/back/mes-avis/index.php
+++ html/back/mes-avis/index.php
@@ -296,10 +296,7 @@ if ($typeCompte === 'proPrive') {
 
                                 $compteur = 0; ?>
                         <?php foreach ($avis as $lavis) {  ?>
-                            
-                            
-
-                            <div class="fond-blocs-avis <?php echo ($lavis['lu'] == false) ? 'avis-en-exergue' : ''; ?>">
+                            <article>
                             <?php if ($lavis['lu'] == false) {
                                                             echo '<div role="tooltip" id="infobulle">Nouveau !</div>';
                                                         } else { //si l'avis a ete lu on met sil a une reponse ou pas
@@ -309,6 +306,9 @@ if ($typeCompte === 'proPrive') {
                                                             }
                                                         }
                                 ?>
+
+                            <div class="fond-blocs-avis <?php echo ($lavis['lu'] == false) ? 'avis-en-exergue' : ''; ?>">
+                            
 
 
                                 <div class="display-ligne-espace">
@@ -346,7 +346,7 @@ if ($typeCompte === 'proPrive') {
                                 <br>
                                 <a href="/back/consulter-offre/index.php?id= <?php echo $id_offre . '#avis' ?>"> Voir à l&#39;avis </a>
                             </div>
-
+                            </article>
                         <?php $compteur++;
                         } ?>
                     <br>
