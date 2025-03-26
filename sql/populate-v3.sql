9ce816ae9ff93997b71f5b9313f617864668eacd
jocooper <jocooper2018@gmail.com>
2025-03-26 16:43:58 +0100

    update populate-v3.sql Le Coste Mor

--- sql/populate-v3.sql
+++ sql/populate-v3.sql
@@ -75,19 +75,27 @@ BEGIN
         "site_web", 
         "id_compte_professionnel", 
         "id_adresse", 
-        "abonnement"
+        "abonnement",
+        "nb_jetons",
+        "jeton_perdu_le",
+        "lat",
+        "lon"
     ) 
     VALUES(
         '€€',
         'https://cdn.website.dish.co/media/be/37/8288528/Galettes-et-crepes.pdf',
         'La Krampouzerie Lannion',
-        'Bienvenue à La Krampouzerie, située en face de la gare et à deux pas du cinéma à Lannion. Nous vous accueillons dans une ambiance contemporaine, animée d''expositions d''artistes locaux.',
+        'Crêperie',
         'Lannion',
         'Bienvenue à La Krampouzerie, située en face de la gare et à deux pas du cinéma à Lannion. Nous vous accueillons dans une ambiance contemporaine, animée d''expositions d''artistes locaux. Notre équipe vous accueille dans un cadre convivial afin que vous puissiez déguster nos galettes et nos crêpes préparées de façon traditionnelle, à la minute avec des préparations « maison » à base de produits locaux. Vous  pourrez déguster l’incontournable complète mais vos papilles seront sans doute interpellées par d''autres associations de saveurs. Sans oublier notre sélection de boissons, y compris des cidres bretons, des bières artisanales ainsi que du vin français. Pour finir votre repas en douceur, on n''oublie pas les crêpes et les glaces artisanales. L''équipe de La Krampouzerie vous attend donc pour un repas décontracté en famille ou entre amis.',
         'https://la-krampouzerie-creperie-lannion.eatbu.com/',
         var_id_compte,
         var_id_adresse,
-        'standard'
+        'standard',
+        NULL,
+        NULL,
+        NULL,
+        NULL
     )
     RETURNING "id_offre" INTO var_id_offre;
 
@@ -105,6 +113,103 @@ BEGIN
     VALUES (
         var_id_offre,
         'LA-KRAMPOUZERIE-IMG-0555-JPG.jpg'
+    );
+
+
+    /* ##################################################################### */
+    /*                             Le Coste Mor                              */
+    /* ##################################################################### */
+
+    INSERT INTO sae._adresse (
+        "num_et_nom_de_voie",
+        "complement_adresse",
+        "code_postal",
+        "ville",
+        "pays"
+    )
+    VALUES (
+        '162 Rue Saint-Guirec',
+        NULL,
+        '22700',
+        'Perros-Guirec',
+        'France'
+    )
+    RETURNING "id_adresse" INTO var_id_adresse;
+
+    INSERT INTO sae.compte_professionnel_prive (
+        "nom_compte",
+        "prenom",
+        "email",
+        "tel",
+        "mot_de_passe",
+        "denomination",
+        "a_propos",
+        "site_web",
+        "id_adresse",
+        "siren"
+    )
+    VALUES (
+        'Vanessa',
+        'Hardouin',
+        'reception@hotelsaint-guirec.com',
+        '+33296914089',
+        '$2y$10$3MnsgiZZSTIPyKdvQVGRdOtyWBo0kYZIG2OnXeq3Sh9YfsKMBhGze', -- 'Mot de passe de Hotel Saint Guirec'
+        'Hotel Saint Guirec',
+        'Hotel',
+        'https://la-krampouzerie-creperie-lannion.eatbu.com/',
+        var_id_adresse,
+        '22700'
+    )
+    RETURNING "id_compte" INTO var_id_compte;
+
+    INSERT INTO sae.offre_restauration (
+        "gamme_prix", 
+        "carte", 
+        "titre", 
+        "resume", 
+        "ville", 
+        "description_detaille", 
+        "site_web", 
+        "id_compte_professionnel", 
+        "id_adresse", 
+        "abonnement",
+        "nb_jetons",
+        "jeton_perdu_le",
+        "lat",
+        "lon"
+    )
+    VALUES (
+        '€€',
+        'https://www.alacarte.direct/menu-restaurant/menu/restaurant-le-coste-mor/58-restaurant-le-coste-mor',
+        'Le Coste Mor',
+        'Restaurant de fruits de mer',
+        'Lannion',
+        'Face au château de Costaérès et la plage de Saint-Guirec, venez déguster un plateau de fruits de mer, savourez un délicieux plat de poisson ou goûter au plaisir simple des traditionnelles moules-frites bretonnes. Notre carte vous réserve d’autres belles surprises, n’attendez plus pour réserver !',
+        'https://www.hotelsaint-guirec.com/restaurants/le-coste-mor/',
+        var_id_compte,
+        var_id_adresse,
+        'premium',
+        3,
+        NULL,
+        NULL,
+        NULL
+    )
+    RETURNING "id_offre" INTO var_id_offre;
+
+    INSERT INTO sae._image (
+        "lien_fichier"
+    )
+    VALUES (
+        'WEB_restaurant_saint_guirec-34.jpg'
+    );
+
+    INSERT INTO sae._offre_contient_image (
+        "id_offre",
+        "id_image"
+    )
+    VALUES (
+        var_id_offre,
+        'WEB_restaurant_saint_guirec-34.jpg'
     );
 
 END $$;
