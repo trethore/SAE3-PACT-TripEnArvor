a6faedc8e2a4f6f5d6d80474055c1b398d66770e
capoupon <camille.poupon@etudiant.univ-rennes.fr>
2025-03-31 17:51:56 +0200

    test printr avec les avis trié (normalement)

--- html/back/mes-avis/index.php
+++ html/back/mes-avis/index.php
@@ -212,9 +212,50 @@ if ($typeCompte === 'proPrive') {
                     }
                 }
 
-                foreach ($touslesavis as $key => $lavis) {
-                    //on tri les avis en fonction de la date de publi de l'article
+                function array_sort($array, $on, $order=SORT_ASC)
+                {
+                    $new_array = array();
+                    $sortable_array = array();
+
+                    if (count($array) > 0) {
+                        foreach ($array as $k => $v) {
+                            if (is_array($v)) {
+                                foreach ($v as $k2 => $v2) {
+                                    if ($k2 == $on) {
+                                        $sortable_array[$k] = $v2;
+                                    }
+                                }
+                            } else {
+                                $sortable_array[$k] = $v;
+                            }
+                        }
+
+                        switch ($order) {
+                            case SORT_ASC:
+                                asort($sortable_array);
+                            break;
+                            case SORT_DESC:
+                                arsort($sortable_array);
+                            break;
+                        }
+
+                        foreach ($sortable_array as $k => $v) {
+                            $new_array[$k] = $array[$k];
+                        }
+                    }
+
+                    return $new_array;
                 }
+                
+                $aviTriéPluRecent[] = array_merge([],$touslesavis);
+                $aviTriéPluAncien[] = array_merge([],$touslesavis);
+                echo "<br>";
+                print("le tri plus recent");
+                print_r(array_sort($aviTriéPluRecent, 'dateAvis', SORT_DESC));
+                echo "<br>";
+                print("le tri plus ancien");
+                print_r(array_sort($aviTriéPluRecent, 'dateAvis', SORT_DESC));
+                
 
                 print_r($touslesavis);
 
