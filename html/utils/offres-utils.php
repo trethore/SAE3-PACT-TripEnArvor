6163b14e1a4c470423af571bc170a518bfce8e7f
capoupon <camille.poupon@etudiant.univ-rennes.fr>
2025-04-01 13:48:34 +0200

    ajout fonction pour avoir la date de publication d'un avis avec le membre et l'offre dans utils

--- html/utils/offres-utils.php
+++ html/utils/offres-utils.php
@@ -671,6 +671,26 @@
         }
     }
 
+
+    // ==== Fonction qui la date de publication d'un avis avec l'id du membre et de l'offre
+    function getDatePublicationAvecIDMembre($id_offre, $id_membre) {
+        global $driver, $server, $dbname, $user, $pass;
+        $reqDatePublication = "SELECT * FROM _avis  JOIN _date on  _avis.publie_le = _date.id_date where id_offre = :id_offre and id_membre = :id_membre;";
+        try {
+            $conn = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
+            $conn->prepare("SET SCHEMA 'sae';")->execute();
+            $stmtDatePublication = $conn->prepare($reqDatePublication);
+            $stmtDatePublication->bindParam(':id_offre', $id_offre, ':id_membre', $id_membre, PDO::PARAM_INT);
+            $stmtDatePublication->execute();
+            $datePublication = $stmtDatePublication->fetchAll(PDO::FETCH_ASSOC);
+            $conn = null;
+            return $datePublication;
+        } catch (Exception $e) {
+            print "Erreur !: " . $e->getMessage() . "<br>";
+            die();
+        }
+    }
+
     function getDateBlacklistage($id_offre, $id_membre) {
         global $driver, $server, $dbname, $user, $pass;
         $reqDateBlacklistage = "SELECT * FROM _avis NATURAL JOIN _blacklister WHERE _blacklister.id_membre = :id_membre AND _blacklister.id_offre = :id_offre";
