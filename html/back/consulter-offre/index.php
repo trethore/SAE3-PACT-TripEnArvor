64ab5dc55047d86727f75519723782c21813c119
StanislasROLLAND <stanislasrolland05@gmail.com>
2025-04-01 14:24:51 +0200

    Test tags

--- html/back/consulter-offre/index.php
+++ html/back/consulter-offre/index.php
@@ -368,7 +368,10 @@ try {
                 if (!empty($tags)) {
                     foreach ($tags as $tag) { 
                 ?>
-                        <p class="tag"><?php echo htmlentities($tag['nom_tag']); ?></p>
+                        <div class="display-ligne-espace">
+                            <p class="tag"><?php echo htmlentities($tag['nom_tag']); ?></p>
+                            <img src="/images/universel/incones/<?php echo htmlentities($tag['nom_tag']); ?>-orange.png" alt="<?php echo htmlentities($tag['nom_tag']); ?>">
+                        </div>
                 <?php 
                     }
                 } else { 
